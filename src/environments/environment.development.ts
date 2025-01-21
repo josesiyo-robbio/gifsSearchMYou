d25102b484bcd1fd4d97dc470652c471f02a9e72
josesiyo-robbio <jose.robles.bo@gmail.com>
2025-01-21 03:13:15 -0600

    changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@ import {HttpParams} from '@angular/common/http';
 export const environment = {
 
   production: false,
-  apiUrlGifs: 'http://api.giphy.com/v1/gifs',
+  apiUrlGifs: 'https://api.giphy.com/v1/gifs',
   apiGifsKeys : 'ISAsRt9qBfbzIhpQwK6ppoJRZZhOz2i8',
   featureFlag: true
 };
